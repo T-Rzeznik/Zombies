33f3ee7c2ac44f60ee8d1b761fd4e1dc5af936b7
T-Rzeznik <trzeznik29@gamil.com>
2024-01-28 19:34:02 -0500

    Added game tick

--- source.py
+++ source.py
@@ -1,21 +1,26 @@
 import pygame
+import os
+
 pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HIEGHT = 600
-
+fps = 60
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIEGHT))
 
+# IMG = pygame.image.load('os.path.join('Assets','filename.png')')   importing img
+# IMG = pygame.transform.scale(filename.png, (width, height))        scaling img
 
 player = pygame.Rect((300,250,50,50)) #(x,y(positions), height,width (of box))
-
-
+clock = pygame.time.Clock() 
 
 run = True
 while run :
     screen.fill((0,0,0)) #starts the loop with a black screen so after each update, the screen returns black
-
+    clock.tick(fps)
+    
+    #screen.blit('filename.png', (x,y) ) // draw png
     
     pygame.draw.rect(screen, (255,0,0), player) #draws the player onscreen
 
